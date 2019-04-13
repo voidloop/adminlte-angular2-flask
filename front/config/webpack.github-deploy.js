eb773c1d107eb32b3553da48ea7aa3b5a72dec54
Marco E <marco.esposito@gmail.com>
2019-04-14 00:49:56 +0200

    deploy?

--- front/config/webpack.github-deploy.js
+++ front/config/webpack.github-deploy.js
@@ -3,7 +3,7 @@
  */
 const fs = require('fs');
 const path = require('path');
-const helpers = require('./helpers');
+//const helpers = require('./helpers');
 const ghDeploy = require('./github-deploy');
 const webpackMerge = require('webpack-merge'); // used to merge webpack configs
 
@@ -14,7 +14,7 @@ const GIT_REMOTE_NAME = 'origin';
 const COMMIT_MESSAGE = 'Updates';
 const GH_REPO_NAME = ghDeploy.getRepoName(GIT_REMOTE_NAME);
 
-module.exports = function (options) {
+module.exports = function(options) {
   const webpackConfigFactory = ghDeploy.getWebpackConfigModule(options); // the settings that are common to prod and dev
   const webpackConfig = webpackConfigFactory(options);
 
@@ -36,23 +36,23 @@ module.exports = function (options) {
       * Prefixing so every resource will be absolute (otherwise it will be url.com/repoName/repoName...
       * Suffixing since chunks will not do it automatically (testes against about page)
       */
-     publicPath: '/' + GH_REPO_NAME + '/' + ghDeploy.safeUrl(webpackConfig.output.publicPath)
+     publicPath: '/' + GH_REPO_NAME + '/' + ghDeploy.safeUrl(webpackConfig.output.publicPath),
    },
 
    plugins: [
      function() {
-       this.plugin('done', function(stats) {
+       this.plugin('done', function() {
          console.log('Starting deployment to GitHub.');
 
-         const logger = function (msg) {
+         const logger = function(msg) {
            console.log(msg);
          };
 
          const options = {
+           dotfiles: true, // for .nojekyll,
            logger: logger,
-           remote: GIT_REMOTE_NAME,
            message: COMMIT_MESSAGE,
-           dotfiles: true // for .nojekyll
+           remote: GIT_REMOTE_NAME,
          };
 
          // Since GitHub moved to Jekyll 3.3, their server ignores the "node_modules" and "vendors" folder by default.
@@ -71,7 +71,7 @@ module.exports = function (options) {
            }
          });
        });
-     }
-   ]
+     },
+   ],
  });
 };
